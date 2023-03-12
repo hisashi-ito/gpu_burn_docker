b2d804b3262aad0992f511366dae12dfacd401fc
Hisashi Itoh <it.hisas@gmail.com>
2023-03-12 23:00:12 +0900

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 IMAGE="gpu-burn"
 CONTAINER="gpu-burn"
-sudo docker run -tid --gpus all --privileged -v /data:/data --name ${CONTAINER} ${IMAGE} /sbin/init
+sudo docker run -tid --gpus all --privileged --name ${CONTAINER} ${IMAGE} /sbin/init
